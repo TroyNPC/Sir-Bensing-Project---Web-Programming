79f1687c998228ab3b8ec58524cb62e2e1fd4c04
Troy_Grefalde <grefaldetroy133@gmail.com>
2025-09-24 20:16:09 +0800

    This is my first push

---
+++ assets/app.js
@@ -0,0 +1,34 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+console.log('yes')
+
+const links = [document.getElementById('homebutton'),
+document.getElementById('aboutbutton'),
+document.getElementById('homebutton'),
+document.getElementById('aboutbutton'),
+document.getElementById('contact')
+];
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const navbarCollapse = document.getElementById("navbarContent");
+    const navLinks = document.querySelectorAll(".nav-link, .btn, .form-control");
+
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        if (navbarCollapse.classList.contains("show")) {
+          new bootstrap.Collapse(navbarCollapse).hide();
+        }
+      });
+    });
+  });
+
