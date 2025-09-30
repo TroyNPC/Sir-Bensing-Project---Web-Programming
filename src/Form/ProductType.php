d5609f58668e301e007a8f33b5255bb911e368e2
Troy_Grefalde <grefaldetroy133@gmail.com>
2025-09-30 20:56:48 +0800

    This has new stuff in it

---
+++ src/Form/ProductType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price', Choicetype::class, [
+                'choices'  => [
+                    '10' => 10,
+                    '20' => 20,
+                    '30' => 30,
+                    '40' => 40,
+                    '50' => 50,
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
